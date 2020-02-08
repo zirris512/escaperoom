f8aeb91df9ab9e56e6687273186e5e854598b649
Brent <mina512@live.com>
2020-02-08 10:40:48 -0600

    edited sql config to hide user/pass with .env

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
-{
+require("dotenv").config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "dev",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": "escaperoom",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "define": {
