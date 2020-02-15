886f5790f269bad3fb7ae801e3e3d56768cf6f81
jdrenteria <jesusrenteria48@gmail.com>
2020-02-15 13:04:53 -0600

    added home.js and home.scss files

---
+++ client/src/pages/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'pages/home.scss';
+import 'phaser/config.js'
+
+
+function home () {
+    return (
+    <div>
+      <div id="game"></div>
+    </div>
+  );
+  }
+
+
+  export default home
+
+
