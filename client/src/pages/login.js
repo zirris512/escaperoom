e686dcfaf4b5586424b756e6b934658c54e3cf75
jdrenteria <jesusrenteria48@gmail.com>
2020-02-18 20:52:30 -0600

    added new login page as well as background to pages and routed it

---
+++ client/src/pages/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'pages/login.scss';
+
+
+
+function login () {
+    return (
+        <div className='login'>
+        
+        </div>
+
+      
+    
+  );
+  }
+
+
+    export default login
+
