d959e1e0e82d7499ea6252a2d3b9937e3ffc894d
jdrenteria <jesusrenteria48@gmail.com>
2020-02-18 20:16:39 -0600

    added and routed game.js to exact path

---
+++ client/src/pages/game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'pages/game.scss';
+import '../phaser/config'
+
+
+function game () {
+    return (
+        <div className='gamescreen'>
+            
+        </div>
+    )
+}
