53b8b23fc368dc7d87e2c256a3da761229bf63b6
jdrenteria <jesusrenteria48@gmail.com>
2020-02-22 11:49:25 -0600

    added index.js under models and also index.js under routes for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
-const router = require('express').Router();
+
+const express = require('express');
 const apiRoutes = require('./api');
 
+
+// AUTHENTICATION
+
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 // API Routes
 router.use('/api/v1', apiRoutes);
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+  user: req.user,
+}));
 
 module.exports = router;
