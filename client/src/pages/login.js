e1a4d0c327a51e5a6b0dbd8c2cf4b7127dd39598
jdrenteria <jesusrenteria48@gmail.com>
2020-02-25 20:45:06 -0600

    changes to login page styling

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -7,10 +7,11 @@ import {Link} from 'react-router-dom';
 function Login () {
     return (
         <div className='login'>
-           <div className="row1 mt-5">
-    <div className="col1-md-6 m-auto">
-      <div className="card1 card-body">
-        <h1 className="text1-center mb-3"><i className="fas1 fa-sign-in-alt"></i>  Login</h1>
+           <div className="row mt-10">
+    <div className="col-md-6 m-auto">
+      <div className="card card-body">
+        <h2 className="text-center mb-3">
+          <i className="fas fa-sign-in-alt"></i>  Login</h2>
       
         <form action="/users/login" method="POST">
           <div className="form-group">
